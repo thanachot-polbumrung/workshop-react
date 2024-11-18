a264230bd49741c8cdc9f34a2b30c4352a517309
kwanchai.dev <kwanchai.dev@myorder.ai>
2024-11-18 23:18:41 +0700

    [Add] workshop

---
+++ src/workshop/workshop.js
@@ -0,0 +1,11 @@
+// ยิง API https://jsonplaceholder.typicode.com/posts //
+
+// วาด UI post ให้เหมือนเว็ปนี้ https://www.eng.src.ku.ac.th/faculty-members/index.php?type=1
+
+// แสดง title, body และ รูปภาพโดยตัวอักษรตัวแรกของ title ใช้ลิงค์ https://placehold.co/400x400/green/white/png?text=a 
+
+// สามารถกดลบโพสได้
+
+// สามารถ search title ได้
+
+// แสดงผลรวม แต่ละ user มีกี่โพสต์
